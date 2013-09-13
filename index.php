d32c2316b355fd70364dbd5a5415f2a67de4b454
blisz <bliszor@gmail.com>
2013-09-13 15:38:07 +0200

    Start

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: josep
+ * Date: 13/09/13
+ * Time: 15:35
+ * To change this template use File | Settings | File Templates.
+ */
+
+require 'vendor/autoload.php';
